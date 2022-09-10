7fcc097e4ceb18306b90e8d6fbde7496824cfa20
dauzic99 <dauzic99@gmail.com>
2022-09-10 17:49:02 +0800

    revisi

--- resources/views/landing/pages/index.blade.php
+++ resources/views/landing/pages/index.blade.php
@@ -20,9 +20,9 @@
             <div class="section bg-transparent mb-0 pb-0 border-0">
                 <div class="container bg-color-light border-0 rounded-3 p-4 p-md-5">
                     <div class="row justify-content-between align-items-center bottommargin-sm">
-                        <div class="col-lg-7 col-sm-7">
+                        <div class="col-lg-9 col-sm-7">
                             <div class="heading-block border-bottom-0 mb-3">
-                                <h2>peta universitas mulawarman</h2>
+                                <h2 style="text-transform: uppercase;">peta universitas mulawarman</h2>
                             </div>
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.68496265519!2d117.15180871475332!3d-0.4684586996582189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2df67f4bb8b9dcd1%3A0x6fd5d38233261f93!2sUniversitas%20Mulawarman!5e0!3m2!1sid!2sid!4v1661751273714!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                             <br>
@@ -48,22 +48,9 @@
                                 <h5 class="fw-normal mb-1">Kualitas Udara Paling baik</h5>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-sm-5 mt-4 mt-sm-0">
+                        <div class="col-md-12 mt-4 mt-sm-0">
                             <div class="bg-white center px-5 py-3 border">
-                                <div
-                                    class="counter counter-large
-                                    @if ($indeks >= 88.31) text-success
-                                @elseif ($indeks >= 76.61)
-                                text-info
-                                @elseif ($indeks >= 65)
-                                text-warning
-                                @else
-                                text-danger @endif
-
-                                fw-bold">
-                                    <span data-from="0" data-to="{{ $indeks }}" data-refresh-interval="10"
-                                        data-speed="1500"></span>%
-                                </div>
+                            <canvas id="testChart"></canvas>
                                 <div class="line my-2"></div>
                                 <h5 class="fw-normal mb-1">Kualitas Udara Paling buruk</h5>
                             </div>
@@ -74,7 +61,8 @@
                     <div class="row">
                         <div class="col-12">
                             <h3>Informasi Kualitas udara per Fakultas</h3>
-                            <div class="col-lg-3 col-sm-5 mt-4 mt-sm-0">
+                            <div class="row">
+                            <div class="col-md-6 mt-4 mt-sm-0">
                             <div class="bg-white center px-5 py-3 border">
                                 <h5 class="fw-normal mb-1">Fakultas Teknik</h5>
                                 <div class="line my-2"></div>
@@ -92,7 +80,10 @@
                                 </div>
                             </div>
                             </div>
-                            <div class="col-lg-3 col-sm-5 mt-4 mt-sm-0">
+
+                            
+
+                            <div class="col-md-6 mt-4 mt-sm-0">
                             <div class="bg-white center px-5 py-3 border">
                                 <h5 class="fw-normal mb-1">Matematika dan Ilmu Pengetahuan Alam</h5>
                                 <div class="line my-2"></div>
@@ -110,7 +101,12 @@
                                 </div>
                             </div>
                             </div>
-                            <div class="col-lg-3 col-sm-5 mt-4 mt-sm-0">
+                            
+
+                            </div>
+                           
+                            <div class="row mt-3">
+                            <div class="col-md-6 mt-4 mt-sm-0">
                             <div class="bg-white center px-5 py-3 border">
                                 <h5 class="fw-normal mb-1">Fakultas Ilmu Sosial dan Ilmu Politik</h5>
                                 <div class="line my-2"></div>
@@ -128,7 +124,7 @@
                                 </div>
                             </div>
                             </div>
-                            <div class="col-lg-3 col-sm-5 mt-4 mt-sm-0">
+                            <div class="col-md-6 mt-4 mt-sm-0">
                             <div class="bg-white center px-5 py-3 border">
                                 <h5 class="fw-normal mb-1">Fakultas Ekonomi dan Bisnisk</h5>
                                 <div class="line my-2"></div>
@@ -146,7 +142,9 @@
                                 </div>
                             </div>
                             </div>
+                            </div>
                             
+                             
 
                             </div>
                         </div>
@@ -206,4 +204,78 @@
 
 
 @push('js')
+<script src="{{ asset('canvas/js/chart.js') }}"></script>
+    <script src="{{ asset('canvas/js/chart-utils.js') }}"></script>
+<script>
+    
+    $(document).ready(function () {
+        var labels = [
+            "PM10",
+            "CO",
+            "NO2",
+            "O2"
+        ];
+        var data = [
+            {
+                label:"PPM",   
+                data:[20.5,30,70.5,50.6],
+                backgroundColor: ['#009ef7',
+                    '#50CD89',
+        '#F1416C',
+        '#003f5c',],
+                borderColor: ['#009ef7',
+        '#50CD89',
+        '#F1416C',
+        '#003f5c',],
+            },
+            
+        ];
+        // data_gender.forEach((element, i) => {
+        //     data.push({
+        //         label: element.label,
+        //         data: element.avg,
+               
+        //     });
+        // });
+        barChart(labels,data,"testChart","Data Test");
+    });
+    function barChart(labels, datas, element, title) {
+        // console.log(labels);
+        // console.log(datas);
+        // console.log(datas);
+        var ctx = document.getElementById(element).getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: datas
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: title
+                },
+                scales: {
+                    yAxes: [{
+                        gridLines: {
+                            drawBorder: false,
+                            color: '#f2f2f2',
+                        },
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            display: false
+                        },
+                        gridLines: {
+                            display: false
+                        }
+                    }]
+                },
+            }
+        });
+    }
+</script>
 @endpush
